7b15bbcbae8e766cdb99d3c6ba24909fc69034d5
Hachu.developer <65321592+hachuu@users.noreply.github.com>
2020-12-16 14:45:21 +0900

    localStorage 추가

--- src/app/main/menu5/menu5.component.ts
+++ src/app/main/menu5/menu5.component.ts
@@ -11,17 +11,39 @@ export class Menu5Component implements OnInit {
   public inputData: string;       // 검색어
   public translateData;
   public inputLanguage;
+  public loadedData;
 
   constructor(
     private service: RestService,
   ) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    this.loadedData = JSON.parse(localStorage.getItem('saveWord'));
   }
 
   async searchWord() {
     this.inputLanguage = await this.service.getDetectLang(this.inputData);
-    this.translateData = await this.service.getTranslateWord(this.inputData, this.inputLanguage.langCode)
+    this.translateData = await this.service.getTranslateWord(this.inputData, this.inputLanguage.langCode);
+    if (!!this.translateData) {
+      this.saveData(this.inputData, this.translateData.message.result.translatedText);
+    }
   }
 
+  saveData(input: string, result: string) {
+    const object = {input, result};
+    let newArr = [];
+    if (!!this.loadedData) {
+      const saved = this.loadedData;
+      newArr = [...saved];
+      newArr.push(object);
+    } else {
+      newArr.push(object);
+    }
+    console.log(newArr);
+    localStorage.setItem('saveWord', JSON.stringify(newArr));
+  }
 }
